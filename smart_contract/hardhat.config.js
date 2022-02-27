0a80d8e5914b75c38a8a9a52ed72b30130a50401
Arun Jangra <arunjngra89@gmail.com>
2022-02-27 16:38:26 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -8,9 +8,9 @@ module.exports = {
     solidity: "0.8.4",
     networks: {
         rinkeby: {
-            url: "https://eth-rinkeby.alchemyapi.io/v2/NMQTe34rIFeFdPruVpoqjQHhU0G11K5x",
+            url: "alchemy url",
             accounts: [
-                '6e96bd2242a939fe98149cc8e736170555cbd32e102e2d52fe1d004ac04eb356'
+                'account private key'
             ]
         }
     }
